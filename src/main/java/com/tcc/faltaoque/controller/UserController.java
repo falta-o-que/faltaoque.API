0658d19844a2a1bb7762f2e01d824d6a4392f9e4
th-cs <202718515+th-cs@users.noreply.github.com>
2026-09-10 05:26:57 -0300

    feat: add list all users method

---
+++ src/main/java/com/tcc/faltaoque/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.tcc.faltaoque.controller;
+
+import com.tcc.faltaoque.service.UserService;
+import com.tcc.faltaoque.dto.response.UserResponse;
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@Operation(summary = "Listar todos os usuários cadastrados")
+	@GetMapping
+	public ResponseEntity<List<UserResponse>> listAllUsers() {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(userService.listAllUsers());
+	}
+}
---
+++ src/main/java/com/tcc/faltaoque/service/UserService.java
@@ -0,0 +1,24 @@
+package com.tcc.faltaoque.service;
+
+import com.tcc.faltaoque.repository.UserRepository;
+import com.tcc.faltaoque.mapper.UserMapper;
+import com.tcc.faltaoque.dto.response.UserResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	public List<UserResponse> listAllUsers() {
+		return userRepository.findAll()
+			.stream()
+			.map(UserMapper::toEntity)
+			.collect(Collectors.toList());
+	}
+
+}
