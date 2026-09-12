9fb53c0340d88943831e94a525b0ba351fbae69e
th-cs <202718515+th-cs@users.noreply.github.com>
2026-09-12 07:44:25 -0300

    feat: add exception handling for validation

--- src/main/java/com/tcc/faltaoque/exception/RestErrorResponse.java
+++ src/main/java/com/tcc/faltaoque/exception/RestErrorResponse.java
@@ -1,10 +1,16 @@
 package com.tcc.faltaoque.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record RestErrorResponse (
 	LocalDateTime timestamp,
 	int status,
 	String error,
-	String message) {
+	String message,
+	List<String> validationErrors) {
 }
--- src/main/java/com/tcc/faltaoque/exception/RestExceptionHandler.java
+++ src/main/java/com/tcc/faltaoque/exception/RestExceptionHandler.java
@@ -1,11 +1,18 @@
 package com.tcc.faltaoque.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.context.request.WebRequest;
+import org.jspecify.annotations.Nullable;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,10 +20,31 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(UserNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	private RestErrorResponse userNotFoundHandler(UserNotFoundException exception) {
-		return new RestErrorResponse(
-			LocalDateTime.now(),
-			HttpStatus.NOT_FOUND.value(),
-			HttpStatus.NOT_FOUND.name(),
-			exception.getMessage());
+		return RestErrorResponse.builder()
+			.timestamp(LocalDateTime.now())
+			.status(HttpStatus.NOT_FOUND.value())
+			.error(HttpStatus.NOT_FOUND.name())
+			.message(exception.getMessage())
+			.build();
+	}
+
+	@Override
+	protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(
+	MethodArgumentNotValidException ex, HttpHeaders headers,
+	HttpStatusCode status, WebRequest request) {
+
+		List<String> errorList = ex.getBindingResult().getFieldErrors()
+			.stream()
+			.map(error -> error.getField() + " : " + error.getDefaultMessage())
+			.toList();
+
+		RestErrorResponse response = RestErrorResponse.builder()
+			.timestamp(LocalDateTime.now())
+			.status(HttpStatus.BAD_REQUEST.value())
+			.error(HttpStatus.BAD_REQUEST.name())
+			.validationErrors(errorList)
+			.build();
+
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 	}
 }
