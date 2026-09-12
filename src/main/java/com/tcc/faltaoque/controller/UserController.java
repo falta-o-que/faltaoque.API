404fee96382912d6d90e46d9e3ea43f0ebbc7d48
th-cs <202718515+th-cs@users.noreply.github.com>
2026-09-12 01:19:40 -0300

    feat: add list all pantries by user method

--- src/main/java/com/tcc/faltaoque/controller/UserController.java
+++ src/main/java/com/tcc/faltaoque/controller/UserController.java
@@ -2,6 +2,7 @@ package com.tcc.faltaoque.controller;
 
 import com.tcc.faltaoque.service.UserService;
 import com.tcc.faltaoque.dto.response.UserResponse;
+import com.tcc.faltaoque.dto.response.PantryResponse;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,15 @@ public class UserController {
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(userService.findUserById(id));
+	}
+
+	@Operation(summary = "Listar todas as despensas de um único usuário")
+	@GetMapping("{id}/pantries")
+	public ResponseEntity<List<PantryResponse>> listAllPantriesByUser(
+	@PathVariable String id) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(userService.listAllPantriesByUser(id));
 	}
 
 }
--- src/main/java/com/tcc/faltaoque/service/UserService.java
+++ src/main/java/com/tcc/faltaoque/service/UserService.java
@@ -1,8 +1,11 @@
 package com.tcc.faltaoque.service;
 
+import com.tcc.faltaoque.entity.User;
 import com.tcc.faltaoque.repository.UserRepository;
 import com.tcc.faltaoque.mapper.UserMapper;
+import com.tcc.faltaoque.mapper.PantryMapper;
 import com.tcc.faltaoque.dto.response.UserResponse;
+import com.tcc.faltaoque.dto.response.PantryResponse;
 import com.tcc.faltaoque.exception.UserNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,4 +30,13 @@ public class UserService {
 			.orElseThrow(() -> new UserNotFoundException(id)));
 	}
 
+	public List<PantryResponse> listAllPantriesByUser(String id) {
+		User user = userRepository.findById(id)
+			.orElseThrow(() -> new UserNotFoundException(id));
+
+		return user.getPantries()
+			.stream()
+			.map(PantryMapper::toEntity)
+			.collect(Collectors.toList());
+	}
 }
