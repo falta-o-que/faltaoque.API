76ad9928792f6ec5c36105dc59e5167494cdecaa
th-cs <202718515+th-cs@users.noreply.github.com>
2026-09-13 06:48:17 -0300

    feat: add security config

---
+++ src/main/java/com/tcc/faltaoque/config/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.tcc.faltaoque.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/swagger-ui.html", "/swagger-ui/**","/v3/api-docs/**").permitAll()
+				.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults());
+		return httpSecurity.build();
+	}
+
+}
