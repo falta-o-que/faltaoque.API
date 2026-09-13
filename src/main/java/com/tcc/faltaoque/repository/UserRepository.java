5913ad2b12bb6b241a37f98e61cd76d89cc4d448
th-cs <202718515+th-cs@users.noreply.github.com>
2026-09-13 07:07:26 -0300

    feat: add find by email method to repository

--- src/main/java/com/tcc/faltaoque/repository/UserRepository.java
+++ src/main/java/com/tcc/faltaoque/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.tcc.faltaoque.repository;
 
 import com.tcc.faltaoque.entity.User;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+	Optional<User> findByEmail(String email);
 }
